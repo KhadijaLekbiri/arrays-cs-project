b1a0762661622a3553591bd964051bf4cdc1377c
Khadija <lekbirikhadija3@gmail.com>
2025-09-29 18:34:22 +0100

    test

--- src/main/java/samplearrays/CourseNumbersArray.java
+++ src/main/java/samplearrays/CourseNumbersArray.java
@@ -3,6 +3,6 @@ package samplearrays;
 public class CourseNumbersArray {
     public static void main(String[] args) {
         int[] registeredCourses = {1010, 1020, 2080, 2140, 2150, 2160};
-
+        int[] updatedCourses = registeredCourses;
     }
 }
